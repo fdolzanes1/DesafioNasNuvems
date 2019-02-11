a85c9fcff7bf855fd4c8465f46f14a08ec199d7a
fdolzanes1 <fdolzanes1@gmail.com>
2019-02-11 08:52:52 -0400

    first commit

---
+++ Assets/Scripts/Pontuacao.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pontuacao : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text textoPontuacao;
+    private int pontos;
+
+    private AudioSource audioPontuacao;
+
+    private void Awake()
+    {
+        this.audioPontuacao = this.GetComponent<AudioSource>();
+    }
+
+    public void AdicionarPontos()
+    {
+        this.pontos += 1;
+        this.AtualizarTexto();
+        this.audioPontuacao.Play();
+    }
+
+    private void AtualizarTexto()
+    {
+        this.textoPontuacao.text = this.pontos.ToString();
+    }
+
+    public void Reiniciar()
+    {
+        this.pontos = 0;
+        this.AtualizarTexto();
+    }
+}
